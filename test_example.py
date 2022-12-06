a7eecfb9e14c755b9cdf92e2072e0acfd413613a
RyanJudd96 <ryanjudd75@gmail.com>
2022-12-06 16:00:47 +0000

    Added container, formatting, python and test file

---
+++ test_example.py
@@ -0,0 +1,10 @@
+from example import *
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -2) == -1
+    assert add(1000000000000000000, 2) == 1000000000000000002
+
+def test_multiply():
+    assert multiply(2, 4) == 8
+    assert multiply(3 * 4) == 12
