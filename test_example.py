235007d3828cd6918a2edd08e4a9eba0d5c50547
RyanJudd96 <ryanjudd75@gmail.com>
2022-12-06 16:48:09 +0000

    more error correction

--- test_example.py
+++ test_example.py
@@ -6,5 +6,6 @@ def test_add():
     assert add(1000000000000000000, 2) == 1000000000000000002
 
 def test_multiply():
-    assert multiply(2 * 4) == 8
-    assert multiply(3 * 4) == 12
+    assert multiply(2, 4) == 8
+    assert multiply(3, 4) == 12
+    assert multiply(200, 3000) == 600000
